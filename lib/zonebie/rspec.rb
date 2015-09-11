462ec99f8339440986df533d17c573093895f06b
Tom Bowden <tom.b1992@gmail.com>
2015-09-11 16:55:15 +0100

    Print output after rpsec runs

--- lib/zonebie/rspec.rb
+++ lib/zonebie/rspec.rb
@@ -4,4 +4,9 @@ RSpec.configure do |c|
   c.before(:suite) do
     Zonebie.set_random_timezone
   end
+  if ENV["ZONEBIE_INFO"]
+    c.after(:suite) do
+      Zonebie.print_timezone_info
+    end
+  end
 end
